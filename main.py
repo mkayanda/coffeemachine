65d445d1444a1837dfb7c0129c505d71462a9e4f
Moses Kayanda <55259290+mkayanda@users.noreply.github.com>
2020-12-11 17:07:07 -0800

    function to deduct ingredients

--- main.py
+++ main.py
@@ -62,6 +62,13 @@ def is_transaction_successful(money_received, drink_cost):
         print("Sorry that's not enough money. Money refunded")
         return False
 
+def make_coffee(drink_name, order_ingredients):
+    """deduct the required ingredient from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️")
+
+
 is_on = True
 
 while is_on:
@@ -78,4 +85,5 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
-            is_transaction_successful(payment, drink['cost'])
+            if is_transaction_successful(payment, drink['cost']):
+
